f650cb2679a33a7742c12b44a550485c46516599
amtBurgos <amt.burgos@gmail.com>
2018-01-21 03:35:14 +0100

    Fixing compiling errors

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import main from './reducers/main';
+import settings from './reducers/settings';
+import maps from './reducers/maps';
 
-const rootReducer = combineReducers({ main });
+const rootReducer = combineReducers({
+  main,
+  settings,
+  maps,
+});
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
